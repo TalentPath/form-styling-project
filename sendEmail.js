57c1e0bfc3a17868e838245ef6a99ff5cf3a50fd
ChaseFaggard <93607164+ChaseFaggard@users.noreply.github.com>
2021-11-15 17:31:44 -0600

    Add files via upload

--- sendEmail.js
+++ sendEmail.js
@@ -1,6 +1,39 @@
-// Your code to validate your form and send the email will go here!
-// All Fields are required for submission
-// Make sure the Password and Confirm Password Match
 
-// Let the user know if the passwords to not match
-// Also let the user know when the email has been successfully sent
+document.querySelector('#form').addEventListener('submit', e => {
+    
+    // Prevent default refresh
+    e.preventDefault()
+
+    // Initiate variables
+    let errorMsg
+    let formData = new FormData(e.target)
+    let pass = formData.get('password')
+    let pass2 = formData.get('password2')
+    let errorText = document.querySelector('#errorMsg')
+    let successText = document.querySelector('#successMsg')
+
+    // Check password
+    if(pass.length < 6) errorMsg = "The password must be at least 6 characters long"
+    else if(pass != pass2) errorMsg = "The passwords don't match"
+    else if(!/\d/.test(pass)) errorMsg = "The password must have 1 number"
+
+    if(errorMsg != undefined) {
+        successText.style.display = 'none'
+        errorText.innerText = errorMsg
+        errorText.style.display = 'block'
+    }
+    else { // Success - send email
+        errorText.style.display = 'none'
+        emailjs.send("service_iike7xf","template_5m2iexe",{
+            firstName: formData.get('firstName'),
+            lastName: formData.get('lastName'),
+            email: formData.get('email')
+        }).then(() => {
+            form.reset()
+            successText.innerText = "Success! Email sent."
+            successText.style.display = 'block'
+        }, (err) => {
+            console.error(JSON.stringify(err))
+        })
+    }
+})
